738fa91ccb303c0b4555a77825222b3180f880e6
dnazir <darryl.nazir@gmail.com>
2020-03-02 11:31:27 +0200

    Add project files.

---
+++ DigitReaderML/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DigitReaderML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
