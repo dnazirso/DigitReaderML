beee3ec4103467244bda17bbf32dc908cbf3d52e
dnazirso <darryl.nazir@gmail.com>
2020-04-15 15:36:00 +0200

    FeedfowardTests (WIP)

---
+++ NeuralNetworkTests/NetworkTests/FeedfowardTests.cs
@@ -0,0 +1,26 @@
+﻿using NeuralNetwork;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace NeuralNetworkTests.NetworkTests
+{
+    public class FeedfowardTests
+    {
+        [Fact]
+        public void FeedfowardNominalBehavior()
+        {
+            // Arrange
+            var sizes = new List<int> { 3, 2, 1 };
+            var inputs = new float[] { 1f, 1f, 1f };
+            var network = new Network(sizes);
+            network.InitializeInputNeurons(inputs);
+
+            // Act
+            network.Feedfoward();
+
+            // Assert
+            Assert.True(network.Activations[0].mat.Cast<float>().SequenceEqual(inputs));
+        }
+    }
+}
